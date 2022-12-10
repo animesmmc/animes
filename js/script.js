f104c047da551b6be439abb8996f11ffaabd0756
animesmmc <120218907+animesmmc@users.noreply.github.com>
2022-12-10 18:05:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const form = document.querySelector('#form');
+const id = document.querySelector('#idAnime');
 const nomeAnime = document.querySelector('#nomeAnime');
 const episodio = document.querySelector('#episodio');
-const video = document.querySelector('#video')
+const qualidade = document.querySelector('#qualidade')
+const video = document.querySelector('#video');
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
-    const URL = `https://lightspeedst.net/s3/mp4/${nomeAnime.value}/sd/${episodio.value}.mp4`;
-
+    const URL = `https://lightspeedst.net/s${id.value}/mp4/${nomeAnime.value}/${qualidade.value}/${episodio.value}.mp4`;
+    video.src = URL;
 });
